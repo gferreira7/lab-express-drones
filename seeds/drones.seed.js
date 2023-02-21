8d999691c7bcdef9d50bbe1f5a121505a6b9b840
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-02-21 20:11:40 +0000

    lab done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+
+const mongoose = require('mongoose')
+const Drone = require('../models/Drone.model')
+
+const MONGO_URI =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lab-express-drones'
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+]
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+
+    Drone.create(drones)
+      .then((data) => {
+        console.log(`${data.length} drones added to the collection`)
+        mongoose.connection.close()
+      })
+      .catch((err) => {
+        console.error('Error connecting to mongo: ', err)
+      })
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo: ', err)
+  })
